3ecf90679cba73d54b0351f8ba619c1f7a0ccfaf
Archit Gupta <architgupta@berkeley.edu>
2018-01-17 16:39:48 -0800

    master: Added a basic test script to that takes an instance of the BSIM model, runs STEAM on it and compares the resulting approximation with the original model
    Changes committed:
    	new file:   test/t_STEAM_API.m

---
+++ STEAM/test/t_STEAM_API.m
@@ -0,0 +1,30 @@
+fprintf(2, 'Testing STEAM API... \n');
+
+% Transistor model parameters
+m_handle = @bsim4;
+model_id = 'VA_BSIM4_NMOS';
+model    = MOS5TModel2D(m_handle, model_identifier);
+
+% STEAM (discretization/interpolation) parameters
+d_method = 'c';
+d_order  = randi([6, 6], 1, 2);
+d_bounds = cell( 1, 2 );
+
+% Interpolation parameters (Which interpolant to use post-discretization)
+n_pieces = randi([1, 1], 1, 2);
+i_method = 'bli';
+
+for d_i = 1 : size(d_bounds, 2)
+    d_bounds{1, d_i} = linspace(-1, 1, n_pieces(d_i)+1)';
+end
+
+% Initializing a STEAM model
+steam_model = STEAM(model, i_method, d_method, d_order, d_bounds);
+
+% Draw a comparison between the original model and the STEAM generated model to
+% see the differences between the two
+fprintf(2, 'Testing STEAM generated model against original!')
+n_test_pts     = 500;
+n_dims         = cMOD.n_in_dims;
+[~, ~, er_plt] = compareIObjs(n_test_pts, d_bounds, model, steam_model, 'BLI');
+compareIDers(n_test_pts, d_bounds, model, steam_model, 'BLI');
