4f78df49dd4e16227f1d090797c3bf68750ff123
Archit Gupta <architgupta@berkeley.edu>
2018-01-18 16:13:33 -0800

    master: Set up the basic test script so that both 3T and 5T MOS models can be tested conveniently (either as a 3T model, or a 2T model)
    Changes committed:
    	modified:   test/t_STEAM_API.m

--- STEAM/test/t_STEAM_API.m
+++ STEAM/test/t_STEAM_API.m
@@ -3,18 +3,27 @@ fprintf(2, 'Testing STEAM API... \n');
 % Transistor model parameters
 m_handle = @bsim3;
 model_id = 'VA_BSIM3_NMOS';
+
+% Trying a 2D Model
+n_dims   = 2;
 model    = MOS5TModel2D(m_handle, model_id);
+
+% If you want to try a full 3D model
+%{
+n_dims   = 3;
+model    = MOS5TModel(m_handle, model_id); 
+%}
 
 % STEAM (discretization/interpolation) parameters
 d_method = 'c';
-d_order  = randi([6, 6], 1, 2);
-d_bounds = cell( 1, 2 );
+d_order  = randi([6, 6], 1, n_dims);
+d_bounds = cell( 1, n_dims );
 
 % Interpolation parameters (Which interpolant to use post-discretization)
-n_pieces = randi([1, 1], 1, 2);
+n_pieces = randi([1, 1], 1, n_dims);
 i_method = 'bli';
 
-for d_i = 1 : size(d_bounds, 2)
+for d_i = 1 : size(d_bounds, n_dims)
     d_bounds{1, d_i} = linspace(-1, 1, n_pieces(d_i)+1)';
 end
 
